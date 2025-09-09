09d62ee61e085e8f407a1f2526449a017cabd26b
Chris Arnold <Chris.Arnold@nmtafe.wa.edu.au>
2025-09-09 10:40:20 +0800

    multiplication and division

---
+++ multiplication.py
@@ -0,0 +1,30 @@
+def addition_example():
+    first_number = 5
+    second_number = 5
+    addition = first_number + second_number
+    #We should see 10!
+    print(addition)
+
+#addition_example()
+
+def multiplication_example():
+    # we multiply numbers together
+    number = 5
+    add_result = number + number + number + number
+    #We should see 20!
+    multiply_result = number * 4
+    print(add_result)
+    print(multiply_result)
+multiplication_example()
+
+def division_example():
+    # we divide numbers
+    
+    # start with 4 * 5 = 20
+    # division turns this upside-down:
+    # 20 / 5 = 4; and
+    # 20 / 4 = 5
+    multiply_result=20
+    number=5
+    print(multiply_result / number)
+division_example()
